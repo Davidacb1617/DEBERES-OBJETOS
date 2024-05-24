d62ce3129333a9d33ee7c3d897beaf7fbf39b69e
David Correa <dacorrea@sudamericano.edu.ec>
2024-05-24 13:13:16 -0500

    Classwork (To Do List)

---
+++ 2.1- Comunicación/TRABAJO EN CLASE/ToDoList/to-do-list/src/app/pages/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { BoardComponent } from '../../components/board/board.component';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [BoardComponent],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css',
+})
+export class TodoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
